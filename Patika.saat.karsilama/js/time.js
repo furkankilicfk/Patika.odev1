770e1bb2eb85e34fbc0adc59aad3a1957cd96fb2
furkankilicfk <78054012+furkankilicfk@users.noreply.github.com>
2021-03-27 03:28:38 +0300

    odev1

---
+++ Patika.saat.karsilama/js/time.js
@@ -0,0 +1,42 @@
+function yourName() {
+    let name = prompt("Adınız nedir", "lütfen o sikik adınızı giriniz")
+    document.getElementById("perName").innerText= name.toUpperCase()
+    
+}
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var day = date.getDay();
+  
+    if (day == 1) {
+      day = "Pazartesi";
+    } else if (day == 2) {
+      day = "Salı";
+    } else if (day == 3) {
+      day = "Çarşamba";
+    } else if (day == 4) {
+      day = "Perşembe";
+    } else if (day == 5) {
+      day = "Cuma";
+    } else if (day == 6) {
+      day = "Cumartesi";
+    } else if (day == 7) {
+      day = "Pazar";
+    }
+  
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+  
+    var time = h + ":" + m + ":" + s + " " + day;
+    document.getElementById("myClock").innerText = time;
+    document.getElementById("myClock").textContent = time;
+  
+    setTimeout(showTime, 1000);
+  }
+
+  yourName()
+  showTime()
